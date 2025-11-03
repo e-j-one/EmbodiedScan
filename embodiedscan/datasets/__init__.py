5e34e93fe82bbdec62d66ac09392f4e10bd446b1
Jaewon Lee <jwlee2843@gmail.com>
2025-11-04 07:00:40 +0900

    [feat] add scannetpp_2x_dataset

--- embodiedscan/datasets/__init__.py
+++ embodiedscan/datasets/__init__.py
@@ -1,5 +1,6 @@
 from .embodiedscan_dataset import EmbodiedScanDataset
 from .mv_3dvg_dataset import MultiView3DGroundingDataset
+from .scannetpp_2x_dataset import Scannetpp2xDataset
 from .transforms import *  # noqa: F401,F403
 
-__all__ = ['EmbodiedScanDataset', 'MultiView3DGroundingDataset']
+__all__ = ['EmbodiedScanDataset', 'MultiView3DGroundingDataset', 'Scannetpp2xDataset']
